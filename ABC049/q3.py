25371175ba92f7c9a477c2ef59c4a0b646677a67
Lischero <vermouth.lischero@gmail.com>
2017-02-02 12:20:54 +0900

    Modified ABC049/q3.py
    WAだった問題を修正

--- ABC049/q3.py
+++ ABC049/q3.py
@@ -1,26 +1,11 @@
 # -*- coding:utf-8 -*-
 import re
-s = input()
-slist = list(s)
-
-a = 7*(len(re.findall(r"dreamer",s)) - len(re.findall(r"dream",s)))
-b = 5*(len(re.findall(r"dream",s)))
-c = 6*(len(re.findall(r"eraser",s)) - len(re.findall(r"erase",s)))
-d = 5*(len(re.findall(r"erase",s)))
-e = len(slist)
-
-if a == 0 and c == 0:
-    e -= 1
-
-if a < 0:
-    a = 0
-
-if c < 0:
-    c = 0
-
-if e == a+b+c+d:
+S = input()
+a = re.sub(r'eraser', '', S)
+b = re.sub(r'erase', '', a)
+c = re.sub(r'dreamer', '', b)
+d = re.sub(r'dream', '', c)
+if len(d) == 0:
     print("YES")
 else:
     print("NO")
-
-

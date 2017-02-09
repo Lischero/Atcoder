cec256510dc76044a18b339b3141b11c1d6170f1
Lischero <vermouth.lischero@gmail.com>
2017-02-10 00:11:49 +0900

    Modified ABC043/q3.py
    通した。

--- ABC043/q3.py
+++ ABC043/q3.py
@@ -2,6 +2,7 @@
 import math
 N = int(input())
 a = list(map(int,input().split()))
-alpha, beta = (sum(a), sum(list(map(lambda n:n**2, a))))
-beta -= int(alpha**2/N)
-print(beta)
+#alpha, beta = (sum(a), sum(list(map(lambda n:n**2, a))))
+#beta -= int(alpha**2/N)
+alpha = sum([(tmp - round(sum(a)/N))**2 for tmp in a]) 
+print(alpha)

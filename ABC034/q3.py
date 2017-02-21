f0da812316b3f41d94d7e518b8c96da0785730ab
Lischero <vermouth.lischero@gmail.com>
2017-02-22 01:13:01 +0900

    Modified ABC034/q3.py
    フェルマーの小定理使用。TLE消えた。

--- ABC034/q3.py
+++ ABC034/q3.py
@@ -1,7 +1,11 @@
 # -*- coding:utf-8 -*-
+#memo:a^p-1 mod p = 1
+#memo:a^p-2 mod p = a^-1
 import math
 W, H = map(int, input().split())
-#l = [[0 for x in range(W)] for y in range(H)]
-ans = math.factorial(W+H-2) // (math.factorial(W-1) * math.factorial(H-1))
-print(ans%((10**9)+7))
+mod = (10**9)+7
+#ans = math.factorial(W+H-2) // (math.factorial(W-1) * math.factorial(H-1))
+a = math.factorial(W+H-2) % mod
+b = pow((math.factorial(W-1) * math.factorial(H-1)) % mod, mod-2, mod)
+print((a*b)%mod)
 

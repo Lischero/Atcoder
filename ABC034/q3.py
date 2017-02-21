6b84b8c5d2840b258319c4f51ad7b7f8b5514b51
Lischero <vermouth.lischero@gmail.com>
2017-02-21 23:59:25 +0900

    Add ABC034/q3.py
    TLE出ちゃった。

--- ABC034/q3.py
+++ ABC034/q3.py
@@ -1,5 +1,7 @@
 # -*- coding:utf-8 -*-
+import math
 W, H = map(int, input().split())
 #l = [[0 for x in range(W)] for y in range(H)]
-
+ans = math.factorial(W+H-2) // (math.factorial(W-1) * math.factorial(H-1))
+print(ans%((10**9)+7))
 

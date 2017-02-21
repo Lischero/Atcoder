afb8a031f01b35889e486d6ddb6cb37f774a4f58
Lischero <vermouth.lischero@gmail.com>
2017-02-21 23:48:44 +0900

    Add ABC034/q1.py q2.py
    cは回答中

---
+++ ABC034/q1.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+x, y = map(int, input().split())
+if x > y:
+    print("Worse")
+else:
+    print("Better")
+
---
+++ ABC034/q3.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+W, H = map(int, input().split())
+#l = [[0 for x in range(W)] for y in range(H)]
+
+

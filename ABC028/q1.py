d9e5324248710bcdce7e73327ccd97935e9216f1
Lischero <vermouth.lischero@gmail.com>
2017-03-01 19:16:10 +0900

    Add ABC028/q1.py

---
+++ ABC028/q1.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+N = int(input())
+if N <= 59:
+    print('Bad')
+elif N >= 60 and N <= 89:
+    print('Good')
+elif N >= 90 and N <= 99:
+    print('Great')
+else:
+    print('Perfect')

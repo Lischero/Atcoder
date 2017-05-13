23778292180091f1da16b1f8162b21b3408cf581
Lischero <vermouth.lischero@gmail.com>
2017-05-13 18:39:44 +0900

    Modified ABC005/q3.py

--- ABC005/q3.py
+++ ABC005/q3.py
@@ -4,23 +4,22 @@ T, N = (int(input()), int(input()))
 A = list(map(int, input().split()))
 M = int(input())
 B = list(map(int, input().split()))
-soldout = 0
-num = 0
-for tmp in range(M):
-    if soldout == N and tmp != M-1:
-        print("no")
-        sys.exit()
-    for tmp2 in range(num,N):
-        if B[tmp] <= A[tmp2]+T and B[tmp] - A[tmp] >= 0:
-            soldout += 1
-            num = tmp2+1
-            break
-        else:
-            continue
-    if soldout == N and tmp == M-1:
-        print("yes")
-        sys.exit()
-if soldout != M: 
+soldout = num = 0
+if N < M:
     print("no")
+    sys.exit()
 else:
-    print("yes")
+    for tmp in range(M):
+        for tmp2 in range(num,N):
+            if B[tmp] <= A[tmp2]+T and B[tmp] - A[tmp2] >= 0:
+                soldout += 1
+                num = tmp2+1
+                break
+            else:
+                if tmp2 == N-1:
+                    print("no")
+                    sys.exit()
+                continue
+        if soldout == M:
+            print("yes")
+            sys.exit()

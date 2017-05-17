6537bf2bcabbf94d157f6d6152a4aa7394c54143
Lischero <vermouth.lischero@gmail.com>
2017-05-17 19:28:26 +0900

    Modified ABC008/q3.py
    AC

--- ABC008/q3.py
+++ ABC008/q3.py
@@ -1,18 +1,23 @@
 # -*- coding:utf-8 -*-
-import math, itertools
+import math
 N = int(input())
-C = ( int(input()) for tmp in range(N) )
-l = list(itertools.permutations(C))
-a = [ True for tmp in range(N) ]
+C = [ int(input()) for tmp in range(N) ]
 ans = 0
-for tmp in range(len(l)):
+for tmp in range(N):
+    factor= 0
     for tmp2 in range(N):
-        for tmp3 in range(tmp2+1, N):
-            if (l[tmp])[tmp3]%(l[tmp])[tmp2] != 0:
-                pass
-            else:
-                a[tmp3] = not (a[tmp3])
-    ans += a.count(True)
-    a = [ True for tmp in range(N) ]
-ans /= math.factorial(N)
+        if tmp != tmp2 and C[tmp]%C[tmp2] == 0:
+            factor += 1 #対象コインに対して、何個のコインが裏返し要員になるか。
+        else:
+            pass
+    #裏返し要員のコインfactor枚と対象のコインで1枚、計(factor+1)の並び方は(factor+1)!通り存在。
+    #裏返し要員のコインfactor枚中、裏返し要員コインq枚がくるのはfactor_C_q通り。
+    #そして対象コインの左側q枚の組み合わせはq!通り。対象コイン右側の組み合わせは(factor-q)!通り。
+    let1 = math.factorial(factor)
+    let2 = let1*(factor+1)
+    for tmp3 in range(0, factor+1, 2):
+        #let3 = math.factorial(factor-tmp3)
+        #let4 = math.factorial(tmp3)
+        #ans += ((let1/(let3*let4)) * let3 * let4) / let2
+        ans += let1 / let2
 print(ans)

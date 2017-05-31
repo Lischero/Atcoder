97095e382a2439b9f533b477275dba96c6849469
Lischero <vermouth.lischero@gmail.com>
2017-05-31 17:49:08 +0900

    Modified ABC016/q3.py
    accumulate使ってみたり

--- ABC017/q3.py
+++ ABC017/q3.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-import sys
+import sys, itertools, operator
 N, M = map(int, input().split())
 juwels = [ 0 for tmp in range(M) ]
 lrs, ans = ( [], 0 )
@@ -12,22 +12,25 @@ for tmp in range(N):
         juwels[r] -= 1
     else:
         pass
+juwels = list(itertools.accumulate(juwels, operator.add))
+'''
 for tmp in range(len(juwels)-1):
     juwels[tmp+1] += juwels[tmp]
-lrs.sort(key = lambda x:x[2])
+'''
 if 0 in juwels:
     print(ans)
     sys.exit()
 else:
     minimum = min(juwels)
-    for tmp in lrs:
-        if minimum in juwels[tmp[0]-1:tmp[1]]:
-            a = [ juwels[tmp2]-1 for tmp2 in range(tmp[0]-1, tmp[1]) ]
-            juwels[tmp[0]-1:tmp[1]] = a
-            ans -= tmp[2]
+    lrs.sort(key = lambda x:x[2])
+    for tmp,tmp2,tmp3 in lrs:
+        if minimum in juwels[tmp-1:tmp2]:
+            a = [ juwels[tmp4]-1 for tmp4 in range(tmp-1, tmp2) ]
+            juwels[tmp-1:tmp2] = a
+            ans -= tmp3
             minimum -= 1
+            if minimum == 0:
+                break
         else:
             pass
-        if minimum == 0:
-            break
     print(ans)

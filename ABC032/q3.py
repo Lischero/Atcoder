83f259cb42bf3a4916938db0a21bb78b0dbd96e2
Lischero <vermouth.lischero@gmail.com>
2017-06-08 00:31:18 +0900

    Modified ABC032/q3.py
    AC

--- ABC032/q3.py
+++ ABC032/q3.py
@@ -1,28 +1,25 @@
 # -*- coding:utf-8 -*-
 import sys
 N, K = map(int, input().split())
-s = sorted([ int(input()) for tmp in range(N) ])
-a = list(filter(lambda n:n<=K, s))
-if len(a) != 0:
+s = [ int(input()) for tmp in range(N) ]
+if len(list(filter(lambda n:n<=K, s))) != 0:
     if 0 in s:
         print(len(s))
         sys.exit()
     else:
-        b = list(filter(lambda n:n!=1, a))
-        ans = len(a)-len(b)
-        c = 0
+        ans = 0
         right = 0
-        d = 1
-        for left in range(len(b)):
-            while right < len(b) and d*b[right] <= K:
-                if right < left:
-                    right -= 1
-                    break
-                d *= b[right]
+        b = 1
+        for left in range(len(s)):
+            if right < left:
+                right = left
+            while right < len(s) and b*s[right] <= K:
+                b *= s[right]
                 right += 1
-            d /= b[left]
-            if c < right-left:
-                c = right-left
-        print(ans+c)
+            if left != right:
+                b /= s[left]
+            if ans < right-left:
+                ans = right-left
+        print(ans)
 else:
     print(0)

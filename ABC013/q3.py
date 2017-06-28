b16b36a253d068acf3ae1d4611163718334daa98
Lischero <vermouth.lischero@gmail.com>
2017-06-28 20:00:47 +0900

    Add ABC013/q3.py

--- ABC013/q3.py
+++ ABC013/q3.py
@@ -1,18 +1,23 @@
 # -*- coding:utf-8 -*-
+import math
 N, H = map(int, input().split())
 A, B, C, D, E = map(int, input().split())
-L = [ 0 for tmp in range(N+1) ]
-L[0] = H
-ans = 0
-for tmp in range(N):
-    a,b,c = (L[tmp]+B, L[tmp]+D, L[tmp]-E)
-    if c > 0:
-        L[tmp+1] = c
-    elif a > 0 or b > 0:
-        L[tmp+1] = min(a,b)
-        if L[tmp+1] == b:
-            ans += C
-        else:
-            ans += A
-print(L)
-print(ans)
+consume = N*E
+a = H - consume
+b = B + E
+c = D + E
+if a > 0:
+    print(0)
+else:
+    flag1 = 0
+    flag2 = 0
+    if abs(a)%b == 0:
+        flag1 = 1
+    if abs(a)%c == 0:
+        flag2 = 1
+    ans = min(A*(math.ceil(abs(a)/b)+flag1), C*(math.ceil(abs(a)/c)+flag2))
+    for tmp in range(abs(a)//c+1):
+        factor = math.ceil((abs(a)-c*tmp)/b)
+        if c*tmp+b*factor+a > 0:
+            ans = min(ans, C*tmp+A*factor)
+    print(ans)

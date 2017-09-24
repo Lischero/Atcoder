0ca309c0461315fe23e37e720b3b7d8e61ec645e
Lischero <vermouth.lischero@gmail.com>
2017-09-24 16:36:52 +0900

    Improved fes2017QualA/q3.py

--- fes2017QualA/q3.py
+++ fes2017QualA/q3.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+import math, sys
 h,w = map(int, input().split())
 a = [ list(input()) for y in range(h) ]
 table = {}
@@ -10,5 +11,32 @@ for y in range(h):
         else:
             table[a[y][x]] = 1
             table2.append(a[y][x])
-kind = len(table2)
-#わからん．飛ばし．
+
+size2 = math.floor(h/2)+math.floor(w/2)
+size4 = math.floor(h/2)*math.floor(w/2)
+
+for tmp in table2:
+    if table[tmp]%4 == 1 or table[tmp]%4 == 3:
+        table[tmp] -= 1
+        break
+
+for tmp in table2:
+    if table[tmp]%4 == 2:
+        table[tmp] -= 2
+        size2 -= 1
+        if size2 == 0:
+            break
+
+for tmp in table2:
+    if table[tmp]%4 == 0 and table[tmp] != 0:
+        table[tmp] -= 4
+        size4 -= 1
+        if size4 == 0:
+            break
+
+for tmp in table2:
+    if table[tmp] != 0:
+        print("No")
+        sys.exit()
+else:
+    print("Yes")

5ea63c4dbe70abd92c476b9eb4c672af37861f1a
Lischero <vermouth.lischero@gmail.com>
2017-11-02 20:41:07 +0900

    Add codeFestival2015_asapuro_middle/q1.py

---
+++ codeFestival2015_asapuro_middle/q1.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+n,k,m,r = map(int, input().split())
+a = [ int(input()) for tmp in range(n-1) ]
+a.sort()
+a.reverse()
+factors = a[0:k]
+average = sum(factors)/k
+if average >= r:
+    print(0)
+else:
+    factors.pop()
+    ans = r*k-sum(factors)
+    if ans <= m:
+        print(ans)
+    else:
+        print(-1)

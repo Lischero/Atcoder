fa267e8d489575f9c01f7d9c89d380969a4cd3f3
Lischero <vermouth.lischero@gmail.com>
2018-01-08 14:05:37 +0900

    Modified ABC085/q4.py

--- ABC085/q4.py
+++ ABC085/q4.py
@@ -3,23 +3,23 @@ import math
 n, h = map(int, input().split())
 l = []
 ans = 0
+
 for tmp in range(n):
     a, b = map(int, input().split())
-    l.append((tmp,a,b))
+    l.append((a,b))
+
+factor = sorted(l, key=lambda tmp:tmp[1])
+max_a = max(l, key=lambda tmp:tmp[0])
 while h > 0:
-    factor = sorted(l, key= lambda tmp:tmp[1])
-    factor2 = sorted(l, key= lambda tmp:tmp[2])
-    max_a = factor.pop()
-    max_b = factor2.pop()
-    if max_b[2] >= h:
-        h = h-max_b[2]
-        l.pop(max_b[0])
-        ans += 1
-    elif math.ceil((h-max_b[2])/max_a[1]) < math.ceil(h/max_a[1]) and len(l) >= 2:
-        h = h-max_b[2]
-        l.pop(max_b[0])
+    if len(factor) != 0:
+        max_b = factor[-1]
+    else:
+        max_b = (-1,-1)
+    if max_a[0] < max_b[1]:
+        h -= max_b[1]
+        factor.pop()
         ans += 1
     else:
-        ans += math.ceil(h/max_a[1])
-        h = h - max_a[1]*(math.ceil(h/max_a[1]))
-    print(ans)
+        ans += math.ceil(h/max_a[0])
+        h -= max_a[0]*math.ceil(h/max_a[0])
+print(ans)

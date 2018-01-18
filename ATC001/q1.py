c4c696a36a2ac458dc2d84a7e8c727f7ef6565fe
Lischero <vermouth.lischero@gmail.com>
2018-01-18 20:22:02 +0900

    Modified ATC001/q1.py

--- ATC001/q1.py
+++ ATC001/q1.py
@@ -1,35 +1,35 @@
 # -*- coding:utf-8 -*-
+import sys
+sys.setrecursionlimit(1000000)
 def search(y,x):
     global flag, h, w, c, a
-    if y >= h or y < 0 or x >= w or x < 0:
-        return
-    if c[y][x] == '#':
-        return
-    if a[y][x]:
-        return
     a[y][x] = True
     if c[y][x] == 'g':
         flag = True
-    else:
-        pass
-    search(y-1,x)
-    search(y+1,x)
-    search(y,x-1)
-    search(y,x+1)
+        return
+    if y-1 >= 0 and not a[y-1][x]:
+        search(y-1,x)
+    if y+1 < h and not a[y+1][x]:
+        search(y+1,x)
+    if x-1 >= 0 and not a[y][x-1]:
+        search(y,x-1)
+    if x+1 < w and not a[y][x+1]:
+        search(y,x+1)
 
-flag = False
-h, w = map(int, input().split())
-c = [ list(input()) for _ in range(h) ]
-a = [ [ False for _ in range(w) ] for _ in range(h) ]
-for tmp in range(h):
-    if 's' in c[tmp]:
-        b = c[tmp].index('s')
-        d = tmp
-        search(d,b)
-        break
+
+
+if __name__ == "__main__":
+    flag = False
+    h, w = map(int, input().split())
+    c = [ list(input()) for _ in range(h) ]
+    a = [ [ c[y][x] == '#' for x in range(w) ] for y in range(h) ]
+    for tmp in range(h):
+        if 's' in c[tmp]:
+            b = c[tmp].index('s')
+            d = tmp
+            search(d,b)
+            break
+    if flag:
+        print("Yes")
     else:
-        pass
-if flag:
-    print("Yes")
-else:
-    print("No")
+        print("No")

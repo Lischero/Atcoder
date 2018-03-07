22bb14502c560f50661ebd15dd077a8678b1fa9c
Lischero <vermouth.lischero@gmail.com>
2018-03-07 20:45:15 +0900

    Modified ABC089/q3.py

--- ABC089/q3.py
+++ ABC089/q3.py
@@ -4,20 +4,13 @@ if __name__ == "__main__":
     n = int(input())
     s = [ input() for tmp in range(n) ]
     alphabetDic = { alphabet : 0 for alphabet in [ chr(tmp) for tmp in range(ord('A'), ord('A')+26) ] }
-    setsNum = 0
     ans = 0
     for word in s:
         alphabetDic[word[0]] += 1
-    factors = [ tmp for tmp in list(alphabetDic.values()) if tmp > 0 ]
-    for value in factors:
-        if value:
-            setsNum += 1
-        else:
-            pass
-    else:
-        if setsNum <= 2:
-            print(ans)
-            sys.exit()
+    factors = [ alphabetDic[tmp] for tmp in ['M', 'A', 'R', 'C', 'H'] if alphabetDic[tmp] > 0 ]
+    if len(factors) <= 2:
+        print(ans)
+        sys.exit()
     for tmp in range(len(factors)):
         for tmp2 in range(tmp+1, len(factors)):
             for tmp3 in range(tmp2+1, len(factors)):

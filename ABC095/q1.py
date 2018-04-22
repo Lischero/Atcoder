cc74811efb6c209483723ea21c0f8fb199e1abf2
Lischero <vermouth.lischero@gmail.com>
2018-04-22 19:27:25 +0900

    Add ABC095/q1.py

---
+++ ABC095/q1.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+if __name__ == "__main__":
+    total = 700
+    s = list(input())
+    for tmp in s:
+        if tmp == 'o':
+            total += 100
+        else:
+            pass
+    else:
+        print(total)

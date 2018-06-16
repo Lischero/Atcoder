7581923547ffc58707db075f383b285e8d4c933a
Lischero <vermouth.lischero@gmail.com>
2018-06-16 23:28:31 +0900

    Modified ABC100/q3.py
    q3.py is AC

--- ABC100/q3.py
+++ ABC100/q3.py
@@ -6,16 +6,14 @@ def check(num):
         return 1
 
 def process(num):
-    if num == 2:
-        return num*3
-    else:
-        return int(num/2)
+    count = 0
+    while(num%2 == 0):
+        num //= 2
+        count += 1
+    return count
 
 if __name__ == "__main__":
     n = int(input())
-    a = [ tmp for tmp in list(map(int, input().split())) if check(tmp) ]
-    ans = 0
-    while(len(a)):
-        a = [ tmp for tmp in list(map(process, a)) if check(tmp) ]
-        ans += 1
+    a = list(map(process, [ tmp for tmp in list(map(int, input().split())) if check(tmp) ]))
+    ans = sum(a)
     print(ans)

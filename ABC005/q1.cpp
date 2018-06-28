174754f2ccb6305d2ea301800ac6d86da9d15a00
Lischero <vermouth.lischero@gmail.com>
2018-06-28 21:39:54 +0900

    Add ABC005/q1.cpp

---
+++ ABC005/q1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double x, y;
+    cin >> x >> y;
+    cout << floor(y/x) << endl;
+    return 0;
+}

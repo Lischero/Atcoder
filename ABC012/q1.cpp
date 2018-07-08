02472a5c7fc9858a562b5e3ce9c7b0f2799d9cdd
Lischero <vermouth.lischero@gmail.com>
2018-07-08 15:00:00 +0900

    Add ABC012/q1.cpp

---
+++ ABC012/q1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << b << ' ' << a << endl;
+    return 0;
+}

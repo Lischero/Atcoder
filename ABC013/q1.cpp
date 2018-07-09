4977706e3766c794ed115b3de7053cb848350cdc
Lischero <vermouth.lischero@gmail.com>
2018-07-09 20:06:15 +0900

    Add ABC013/q1.cpp

---
+++ ABC013/q1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    char x;
+    cin >> x;
+    cout << x-'A'+1 << endl;
+    return 0;
+}

734dac19c1026f9b073cd2078dce3007e36848c3
Lischero <vermouth.lischero@gmail.com>
2018-07-16 16:49:27 +0900

    Add ABC020/q1.cpp

---
+++ ABC020/q1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int q;
+    cin >> q;
+    if (q == 1){
+        cout << "ABC" << endl;
+    } else {
+        cout << "chokudai" << endl;
+    }
+    return 0;
+}

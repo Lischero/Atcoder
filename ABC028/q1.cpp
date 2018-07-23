512547e9560f1449a9269bd22dd6d6ba2a393590
Lischero <vermouth.lischero@gmail.com>
2018-07-23 19:22:15 +0900

    Add ABC028/q1.cpp

---
+++ ABC028/q1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if ( n <= 59 ){
+        cout << "Bad" << endl;
+    } else if( n <= 89 ){
+        cout << "Good" << endl;
+    } else if ( n <= 99 ){
+        cout << "Great" << endl;
+    } else {
+        cout << "Perfect" << endl;
+    }
+    return 0;
+}

7a3c49a2f531058d18f829496f38c5dff9c8810f
Lischero <vermouth.lischero@gmail.com>
2018-08-11 19:47:11 +0900

    Add ABC045/q1.cpp

---
+++ ABC045/q1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a, b, h;
+    cin >> a;
+    cin >> b;
+    cin >> h;
+    cout << ((a+b)*h)/2 << endl;
+    return 0;
+}

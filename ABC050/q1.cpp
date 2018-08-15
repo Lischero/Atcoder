9e81e754ed6cdbc2b0cc706a6d003316419d855f
Lischero <vermouth.lischero@gmail.com>
2018-08-15 19:11:56 +0900

    Add ABC050/q1.cpp

---
+++ ABC050/q1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a, b;
+    char op;
+    cin >> a >> op >> b;
+    if (op == '+'){
+        cout << a+b << endl;
+    } else {
+        cout << a-b << endl;
+    }
+    return 0;
+}

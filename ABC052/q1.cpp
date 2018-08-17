a5fdf394c6024b688d73ea33366ac9fea2026f4a
Lischero <vermouth.lischero@gmail.com>
2018-08-17 19:11:21 +0900

    Add ABC052/q1.cpp

---
+++ ABC052/q1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (a*b >= c*d){
+        cout << a*b << endl;
+    } else {
+        cout << c*d << endl;
+    }
+    return 0;
+}

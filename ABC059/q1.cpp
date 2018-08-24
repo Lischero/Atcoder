0e94801fe85d4b6dd7c71e878c149563670b9dcc
Lischero <vermouth.lischero@gmail.com>
2018-08-24 11:04:32 +0900

    Add ABC059/q1.cpp

---
+++ ABC059/q1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+    string s1, s2, s3;
+    cin >> s1 >> s2 >> s3;
+    transform(s1.begin(), s1.end(), s1.begin(), ::toupper);
+    transform(s2.begin(), s2.end(), s2.begin(), ::toupper);
+    transform(s3.begin(), s3.end(), s3.begin(), ::toupper);
+    cout << s1[0] << s2[0] << s3[0] << endl;
+    return 0;
+}

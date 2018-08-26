74fcae2b1a3b1230a530d5c5e34c9e9f463e1ea9
Lischero <vermouth.lischero@gmail.com>
2018-08-26 18:57:40 +0900

    Add ABC061/q1.cpp

---
+++ ABC061/q1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a <= c && c <= b){
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+    return 0;
+}

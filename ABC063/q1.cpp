2be7cc16041f62c2537dbe34a8e9aaf01f44ee3a
Lischero <vermouth.lischero@gmail.com>
2018-08-28 20:35:51 +0900

    Add ABC063/q1.cpp

---
+++ ABC063/q1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if (a+b < 10){
+        cout << a+b << endl;
+    } else {
+        cout << "error" << endl;
+    }
+    return 0;
+}

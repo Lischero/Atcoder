1ec22dd0129ff5b2821f3354ee53e2fd0ce1582c
Lischero <vermouth.lischero@gmail.com>
2018-08-29 17:28:02 +0900

    Add ABC064/q1.cpp

---
+++ ABC064/q1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int r, g, b;
+    cin >> r >> g >> b;
+    if ((r*100+g*10+b)%4){
+        cout << "NO" << endl;
+    } else {
+        cout << "YES" << endl;
+    }
+    return 0;
+}

5eeae27f217f161f68d3f04a65b4a5e1074031b8
Lischero <vermouth.lischero@gmail.com>
2018-09-18 18:15:20 +0900

    Add ABC083/q1.cpp

---
+++ ABC083/q1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (a+b < c+d){
+        cout << "Right" << endl;
+    } else if(a+b > c+d){
+        cout << "Left" << endl;
+    } else {
+        cout << "Balanced" << endl;
+    }
+    return 0;
+}

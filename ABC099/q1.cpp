e496245b22c30454c212a916b114116c273cc5fa
Lischero <vermouth.lischero@gmail.com>
2018-10-05 15:42:20 +0900

    Add ABC099/q1.cpp

---
+++ ABC099/q1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n < 1000){
+        cout << "ABC" << endl;
+    } else {
+        cout << "ABD" << endl;
+    }
+    return 0;
+}

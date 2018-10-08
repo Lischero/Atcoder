b250f3cbeac335cb678b16786add77ade0de676d
Lischero <vermouth.lischero@gmail.com>
2018-10-08 18:29:13 +0900

    Add ABC102/q1.cpp

---
+++ ABC102/q1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n%2){
+        cout << n*2 << endl;
+    } else {
+        cout << n << endl;
+    }
+    return 0;
+}

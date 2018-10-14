35ba4ebb6e2720710c84a69ac1b50ff7083e99bc
Lischero <vermouth.lischero@gmail.com>
2018-10-14 19:35:09 +0900

    Add ABC108/q1.cpp

---
+++ ABC108/q1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int k;
+    cin >> k;
+    cout << (int)floor(k/2.0)*(int)ceil(k/2.0) << endl;
+    return 0;
+}

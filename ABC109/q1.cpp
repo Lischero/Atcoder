bae8a5ff821ca5075d92e63ffb16f8f38659f9f6
Lischero <vermouth.lischero@gmail.com>
2018-10-15 19:23:53 +0900

    Add ABC109/q1.cpp

---
+++ ABC109/q1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if (a%2 == 0 || b%2 == 0){
+        cout << "No" << endl;
+    } else {
+        cout << "Yes" << endl;
+    }
+    return 0;
+}
